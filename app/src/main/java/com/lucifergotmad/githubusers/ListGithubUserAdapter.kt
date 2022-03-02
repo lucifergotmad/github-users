21d00b1e4a7fb073a5cd65c2f2773e741b4d6a0d
L u c i f e r <amateur.fullstackdev@gmail.com>
2022-03-02 20:55:09 +0700

    fix major

--- app/src/main/java/com/lucifergotmad/githubusers/ListGithubUserAdapter.kt
+++ app/src/main/java/com/lucifergotmad/githubusers/ListGithubUserAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ListGithubUserAdapter(private val listGithubUser: ArrayList<GithubUser>) :
     RecyclerView.Adapter<ListGithubUserAdapter.ListViewHolder>() {
@@ -27,7 +28,7 @@ class ListGithubUserAdapter(private val listGithubUser: ArrayList<GithubUser>) :
     override fun getItemCount(): Int = listGithubUser.size
 
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val userAvatar: ImageView = itemView.findViewById(R.id.user_avatar)
+        val userAvatar: CircleImageView = itemView.findViewById(R.id.user_avatar)
         val userName: TextView = itemView.findViewById(R.id.tv_user_name)
         val fullName: TextView = itemView.findViewById(R.id.tv_full_name)
     }
